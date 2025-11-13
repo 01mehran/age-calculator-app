a10a9ce7d0edb82d0179d7293bc1c967af6a964d
mehran <mehranmohamadi1311@gmail.com>
2025-11-14 01:04:23 +0330

    feat(erros): implement error handling for  input fields

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,11 @@
 import { useId } from "react";
 
-const Input = ({ label, placeholder, value, onChange, name }) => {
+const Input = ({ label, placeholder, value, onChange, name, error }) => {
   const ID = useId();
 
   return (
-    <article className="flex w-full max-w-30 flex-col space-y-1">
-      <label htmlFor={ID} className="label">
+    <article className="relative flex w-full max-w-30 flex-col space-y-1 pb-5">
+      <label htmlFor={ID} className={`label ${error ? "text-red" : ""}`}>
         {label}
       </label>
 
@@ -13,11 +13,14 @@ const Input = ({ label, placeholder, value, onChange, name }) => {
         type="number"
         placeholder={placeholder}
         id={ID}
-        className="input outline-purple focus:outline"
+        className={`input outline-purple focus:outline ${error ? "border-red focus:outline-0" : ""}`}
         value={value}
         onChange={onChange}
         name={name}
       />
+      <span className="text-red font-meduim small:bottom-0 small:text-[11px] absolute bottom-1.5 text-[8px] text-nowrap">
+        {error}
+      </span>
     </article>
   );
 };
--- src/pages/AgeCalculatorBox.jsx
+++ src/pages/AgeCalculatorBox.jsx
@@ -2,29 +2,35 @@
 import { useReducer } from "react";
 
 // Components;
-import Age from "../components/Age";
-import Input from "../components/Input";
+import Age from "@/components/Age";
+import Input from "@/components/Input";
 
 // Images;
-import arrowIcon from "../assets/images/icon-arrow.svg";
+import arrowIcon from "@images/icon-arrow.svg";
 
 const initialState = {
   day: "",
   month: "",
   year: "",
+  dayError: "",
+  monthError: "",
+  yearError: "",
 };
 
 function reducer(state, action) {
   switch (action.type) {
     case "input/change":
       return { ...state, [action.payload.name]: action.payload.value };
+    case "input/empty":
+      return { ...state, [action.payload.name]: action.payload.message };
     default:
       return state;
   }
 }
 
 function AgeCalculatorBox() {
-  const [{ day, month, year }, dispatch] = useReducer(reducer, initialState);
+  const [{ day, month, year, dayError, monthError, yearError }, dispatch] =
+    useReducer(reducer, initialState);
 
   // Update input fields;
   function handleInputChange(e) {
@@ -35,19 +41,79 @@ function AgeCalculatorBox() {
         value: e.target.value,
       },
     });
+
+    // Delete error when user start typing;
+    dispatch({
+      type: "input/empty",
+      payload: { name: `${e.target.name}Error`, message: "" },
+    });
+  }
+
+  // Sybmit;
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const currentDate = new Date().getFullYear();
+    console.log(currentDate);
+    // Day;
+    if (!day) {
+      dispatch({
+        type: "input/empty",
+        payload: { name: "dayError", message: "This field is required" },
+      });
+    } else if (day < 1 || day > 31) {
+      dispatch({
+        type: "input/empty",
+        payload: { name: "dayError", message: "Must be a valid day" },
+      });
+    }
+    // Month;
+    if (!month) {
+      dispatch({
+        type: "input/empty",
+        payload: { name: "monthError", message: "This field is required" },
+      });
+    } else if (month < 1 || month > 12) {
+      dispatch({
+        type: "input/empty",
+        payload: { name: "monthError", message: "Must be a valid month" },
+      });
+    }
+
+    // Year;
+    if (!year) {
+      dispatch({
+        type: "input/empty",
+        payload: { name: "yearError", message: "This field is required" },
+      });
+    } else if (year < 1900) {
+      dispatch({
+        type: "input/empty",
+        payload: { name: "yearError", message: "Must be greater 1900" },
+      });
+    } else if (year > currentDate) {
+      dispatch({
+        type: "input/empty",
+        payload: { name: "yearError", message: "Must be in the past" },
+      });
+    }
   }
 
   return (
     <div className="flex h-dvh items-center justify-center">
       <section className="small:pl-8 mx-2 w-full max-w-152 rounded-2xl rounded-br-[5rem] bg-white px-4 py-8 shadow-md">
         {/* Input fields */}
-        <form className="mb-12 flex items-center space-x-5">
+        <form
+          className="small:space-x-5 mb-12 flex items-center space-x-2"
+          onSubmit={handleSubmit}
+        >
           <Input
             label="day"
             placeholder="DD"
             value={day}
             onChange={handleInputChange}
             name="day"
+            error={dayError}
           />
           <Input
             label="month"
@@ -55,6 +121,7 @@ function AgeCalculatorBox() {
             value={month}
             onChange={handleInputChange}
             name="month"
+            error={monthError}
           />
           <Input
             label="year"
@@ -62,6 +129,7 @@ function AgeCalculatorBox() {
             value={year}
             onChange={handleInputChange}
             name="year"
+            error={yearError}
           />
           <input type="submit" className="hidden" />
         </form>
@@ -72,6 +140,7 @@ function AgeCalculatorBox() {
             src={arrowIcon}
             alt="icon arrow"
             className="bg-purple small:translate-x-0 small:left-auto small:right-0 small:w-16 small:p-3 hover:bg-black-500 absolute left-1/2 aspect-square w-14 -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full p-3 transition-all duration-300 active:translate-y-px"
+            onClick={handleSubmit}
           />
         </div>
 
