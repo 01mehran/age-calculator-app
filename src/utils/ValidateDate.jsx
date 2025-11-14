0b96b484f04846cabcb6b35d5f781f4632ea903c
mehran <mehranmohamadi1311@gmail.com>
2025-11-15 01:06:22 +0330

    fix(erros): fix errors handling

--- src/utils/ValidateDate.jsx
+++ src/utils/ValidateDate.jsx
@@ -1,17 +1,20 @@
 const ValidateDate = ({ day, month, year, dispatch }) => {
   //   Get current year;
   const currentYear = new Date().getFullYear();
+  let hasError = false;
 
   if (!day) {
     dispatch({
       type: "input/empty",
       payload: { name: "dayError", message: "This field is required" },
     });
+    hasError = true;
   } else if (day < 1 || day > 31) {
     dispatch({
       type: "input/empty",
       payload: { name: "dayError", message: "Must be a valid day" },
     });
+    hasError = true;
   }
   // Month;
   if (!month) {
@@ -19,11 +22,13 @@ const ValidateDate = ({ day, month, year, dispatch }) => {
       type: "input/empty",
       payload: { name: "monthError", message: "This field is required" },
     });
+    hasError = true;
   } else if (month < 1 || month > 12) {
     dispatch({
       type: "input/empty",
       payload: { name: "monthError", message: "Must be a valid month" },
     });
+    hasError = true;
   }
 
   // Year;
@@ -32,17 +37,22 @@ const ValidateDate = ({ day, month, year, dispatch }) => {
       type: "input/empty",
       payload: { name: "yearError", message: "This field is required" },
     });
+    hasError = true;
   } else if (year < 1900) {
     dispatch({
       type: "input/empty",
       payload: { name: "yearError", message: "Must be greater 1900" },
     });
+    hasError = true;
   } else if (year > currentYear) {
     dispatch({
       type: "input/empty",
       payload: { name: "yearError", message: "Must be in the past" },
     });
+    hasError = true;
   }
+
+  return !hasError;
 };
 
 export default ValidateDate;
