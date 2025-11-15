af33a23221d427dbfe46853f0257f1eda8113cde
mehran <mehranmohamadi1311@gmail.com>
2025-11-15 15:26:13 +0330

    deploy setup on gh-pages

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/age-calculator-app/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
